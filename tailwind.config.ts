4cdaaa5ac75b238dceaf2b51dec73a5268ad6b47
Hassan Ali <hassanalimalek9@gmail.com>
2025-02-26 15:46:07 +0500

    tailwind configured and required packages installed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,24 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#1B395E", // sky-950
+          hover: "#0F2744",
+        },
+        accent: {
+          DEFAULT: "#CA8A04", // yellow-600
+          hover: "#A16207", // yellow-700
+        },
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
       },
     },
   },
